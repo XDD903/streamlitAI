be344847093619b6dc0f80153dba61461f3c78be
一只小叮当ya <3155799762@qq.com>
2026-06-24 11:30:37 +0800

    Update Fun.py

--- AI/Fun.py
+++ AI/Fun.py
@@ -30,7 +30,6 @@ def LoadSessionPath():
         AllSession_list=[*old_Session_list,PathName]
         json.dump(AllSession_list, fp,ensure_ascii=False,indent=2)
 
-st.success(os.path.join(os.path.dirname(os.path.abspath(__file__)),f"JSON/{SessionName}.json"))
 def LoadSession(SessionName):
     if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),f"JSON/{SessionName}.json")):
         ClearSessionState()
